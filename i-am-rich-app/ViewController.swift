dda35d02a6d3397dfe99c3c9938efb4abe979e5d
malcodeman <26983729+malcodeman@users.noreply.github.com>
2020-07-18 18:44:34 +0200

    Initial Commit

---
+++ i-am-rich-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-am-rich-app
+//
+//  Created by Amer K on 18/07/2020.
+//  Copyright © 2020 Amer K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
